99cc9ec5f866bd5e9945d385400d7f730e803d33
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-08-09 16:37:59 -0400

    1 binaries uploaded

--- components/Stats.vue
+++ components/Stats.vue
@@ -13,21 +13,35 @@
         dark:bg-gray-800 dark:border-gray-700
       "
     >
-      <h5
-        class="
-          mb-2
-          text-sm
-          font-bold
-          tracking-tight
-          text-gray-900
-          dark:text-gray-100
-        "
-      >
-        Top Gainers
-        <div class="float-right text-gray-100">View More</div>
-      </h5>
       <p class="font-normal text-gray-700 dark:text-gray-400">
-      
+          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <tbody>
+            <tr class="bg-white dark:bg-gray-800">
+                <th scope="row" class="px-6 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <img src="https://dynamic-assets.coinbase.com/e785e0181f1a23a30d9476038d9be91e9f6c63959b538eabbc51a1abc8898940383291eede695c3b8dfaa1829a9b57f5a2d0a16b0523580346c6b8fab67af14b/asset_icons/b57ac673f06a4b0338a596817eb0a50ce16e2059f327dc117744449a47915cb2.png"/> Bitcoin
+                </th>
+                <td>
+                    Silver
+                </td>
+            </tr>
+            <tr class="bg-white dark:bg-gray-800">
+                <th scope="row" class="px-6 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Microsoft Surface Pro
+                </th>
+                <td>
+                    White
+                </td>
+            </tr>
+            <tr class="bg-white dark:bg-gray-800">
+                <th scope="row" class="px-6 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Magic Mouse 2
+                </th>
+                <td>
+                    Black
+                </td>
+            </tr>
+        </tbody>
+    </table>
       </p>
     </span>
   </div>
