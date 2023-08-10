cd1a5046cb18b95885a1304d4d902b347ade3cf2
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-08-09 20:58:53 -0400

    1 file modified, 1 added, 1 deleted

--- components/Stats.vue
+++ components/Stats.vue
@@ -1,48 +1,182 @@
 <template>
-  <div class="grid grid-cols-3 gap-4 mt-10">
-    <span
-      href="#"
-      class="
-        block
-        max-w-sm
-        p-3
-        bg-white
-        border border-gray-200
-        rounded-lg
-        shadow
-        dark:bg-gray-800 dark:border-gray-700
-      "
-    >
-      <p class="font-normal text-gray-700 dark:text-gray-400">
-          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <tbody>
-            <tr class="bg-white dark:bg-gray-800">
-                <th scope="row" class="px-6 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    <img src="https://dynamic-assets.coinbase.com/e785e0181f1a23a30d9476038d9be91e9f6c63959b538eabbc51a1abc8898940383291eede695c3b8dfaa1829a9b57f5a2d0a16b0523580346c6b8fab67af14b/asset_icons/b57ac673f06a4b0338a596817eb0a50ce16e2059f327dc117744449a47915cb2.png"/> Bitcoin
-                </th>
-                <td>
-                    Silver
-                </td>
-            </tr>
-            <tr class="bg-white dark:bg-gray-800">
-                <th scope="row" class="px-6 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Microsoft Surface Pro
-                </th>
-                <td>
-                    White
-                </td>
-            </tr>
-            <tr class="bg-white dark:bg-gray-800">
-                <th scope="row" class="px-6 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Magic Mouse 2
-                </th>
-                <td>
-                    Black
-                </td>
-            </tr>
-        </tbody>
-    </table>
-      </p>
-    </span>
+  <div
+    class="
+      w-[95%]
+      bg-gray-900
+      text-white
+      flex
+      justify-between
+      break-words
+      px-4
+      lg:px-6
+      py-2.5
+      space-x-16
+    "
+  >
+    <!--Top Gainers-->
+    <div class="w-full flex flex-col mr-5">
+      <div class="items-center flex justify-between mb-3.5">
+        <div class="items-center flex">
+          <p class="text-sm font-medium">Top Gainers</p>
+        </div>
+      </div>
+      <div
+        class="
+          bg-gray-900
+          flex flex-col
+          justify-between
+          border border-white/[0.03] border-solid
+          rounded-2xl
+          pb-2
+        "
+      >
+        <div
+          class="
+            items-center
+            cursor-pointer
+            flex
+            justify-between
+            mt-2
+            py-2
+            px-3.5
+          "
+        >
+          <div class="items-center flex">
+            <img
+              src="../assets/crypto/bitcoin.png"
+              class="h-4 mr-2 rounded-full overflow-clip"
+            />
+            <p class="text-sm overflow-x-hidden overflow-y-auto text-ellipsis">
+              Bitcoin
+            </p>
+          </div>
+          <div class="items-center flex ml-3">
+            <svg
+              viewbox="0 0 24 24"
+              focusable="false"
+              fill="currentColor"
+              class="h-4 w-4 text-emerald-500 mr-1"
+            >
+              <path
+                fill="currentColor"
+                d="M12.8,5.4c-0.377-0.504-1.223-0.504-1.6,0l-9,12c-0.228,0.303-0.264,0.708-0.095,1.047 C2.275,18.786,2.621,19,3,19h18c0.379,0,0.725-0.214,0.895-0.553c0.169-0.339,0.133-0.744-0.095-1.047L12.8,5.4z"
+              ></path>
+            </svg>
+            <p class="text-emerald-500 text-sm">77.86%</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--End of Top Gainers-->
+    <!--Newly Listed-->
+    <div class="w-full flex flex-col mr-5">
+      <div class="items-center flex justify-between mb-3.5">
+        <div class="items-center flex">
+          <p class="text-sm font-medium">Recently Added</p>
+        </div>
+      </div>
+      <div
+        class="
+          bg-gray-900
+          flex flex-col
+          justify-between
+          border border-white/[0.03] border-solid
+          rounded-2xl
+          pb-2
+        "
+      >
+        <div
+          class="
+            items-center
+            cursor-pointer
+            flex
+            justify-between
+            mt-2
+            py-2
+            px-3.5
+          "
+        >
+          <div class="items-center flex">
+            <img
+              src="../assets/crypto/bitcoin.png"
+              class="h-4 mr-2 rounded-full overflow-clip"
+            />
+            <p class="text-sm overflow-x-hidden overflow-y-auto text-ellipsis">
+              Bitcoin
+            </p>
+          </div>
+          <div class="items-center flex ml-3">
+            <svg
+              viewbox="0 0 24 24"
+              focusable="false"
+              fill="currentColor"
+              class="h-4 w-4 text-emerald-500 mr-1"
+            >
+              <path
+                fill="currentColor"
+                d="M12.8,5.4c-0.377-0.504-1.223-0.504-1.6,0l-9,12c-0.228,0.303-0.264,0.708-0.095,1.047 C2.275,18.786,2.621,19,3,19h18c0.379,0,0.725-0.214,0.895-0.553c0.169-0.339,0.133-0.744-0.095-1.047L12.8,5.4z"
+              ></path>
+            </svg>
+            <p class="text-emerald-500 text-sm">77.86%</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--End of Newly Listed-->
+    <!--Biggest Lost-->
+    <div class="w-full flex flex-col mr-5">
+      <div class="items-center flex justify-between mb-3.5">
+        <div class="items-center flex">
+          <p class="text-sm font-medium">Biggest Loss</p>
+        </div>
+      </div>
+      <div
+        class="
+          bg-gray-900
+          flex flex-col
+          justify-between
+          border border-white/[0.03] border-solid
+          rounded-2xl
+          pb-2
+        "
+      >
+        <div
+          class="
+            items-center
+            cursor-pointer
+            flex
+            justify-between
+            mt-2
+            py-2
+            px-3.5
+          "
+        >
+          <div class="items-center flex">
+            <img
+              src="../assets/crypto/bitcoin.png"
+              class="h-4 mr-2 rounded-full overflow-clip"
+            />
+            <p class="text-sm overflow-x-hidden overflow-y-auto text-ellipsis">
+              Bitcoin
+            </p>
+          </div>
+          <div class="items-center flex ml-3">
+            <svg
+              viewbox="0 0 24 24"
+              focusable="false"
+              fill="currentColor"
+              class="h-4 w-4 text-emerald-500 mr-1"
+            >
+              <path
+                fill="currentColor"
+                d="M12.8,5.4c-0.377-0.504-1.223-0.504-1.6,0l-9,12c-0.228,0.303-0.264,0.708-0.095,1.047 C2.275,18.786,2.621,19,3,19h18c0.379,0,0.725-0.214,0.895-0.553c0.169-0.339,0.133-0.744-0.095-1.047L12.8,5.4z"
+              ></path>
+            </svg>
+            <p class="text-emerald-500 text-sm">77.86%</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--Biggest Lost-->
   </div>
 </template>
