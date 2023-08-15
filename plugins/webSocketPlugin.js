0cc86513b50781ebed6197f0ecdf6c3b5865ceb3
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-08-15 11:48:04 -0400

    Update webSocketPlugin.js

--- plugins/webSocketPlugin.js
+++ plugins/webSocketPlugin.js
@@ -1,94 +1,92 @@
-import { ref, onMounted, onBeforeUnmount, watch } from 'vue';
+import { ref, onBeforeUnmount, watch } from 'vue';
 import { useIdle } from '@vueuse/core';
 
-export default ({ app }) => {
-  let ws;
-  const webSocketStatus = ref('');
-  const webSocketPing = ref(0);
-  let pingInterval = null;
-  let idleTimeout = null;
+let ws;
+const webSocketStatus = ref('');
+const webSocketPing = ref(0);
+let pingInterval = null;
+let idleTimeout = null;
 
-  const pingWebSocket = () => {
-    if (ws && ws.readyState === WebSocket.OPEN) {
-      const pingTimestamp = new Date().getTime();
-      ws.send(`ping:${pingTimestamp}`);
-    }
-  };
+const pingWebSocket = () => {
+  if (ws && ws.readyState === WebSocket.OPEN) {
+    const pingTimestamp = new Date().getTime();
+    ws.send(`ping:${pingTimestamp}`);
+  }
+};
 
-  const startPingInterval = () => {
-    if (!pingInterval) {
-      pingInterval = setInterval(() => {
-        pingWebSocket();
-      }, 1000);
-    }
-  };
-
-  const stopPingInterval = () => {
-    if (pingInterval) {
-      clearInterval(pingInterval);
-      pingInterval = null;
-    }
-  };
-
-  const handlePongMessage = (event) => {
-    if (event.data.startsWith('pong:')) {
-      const [, pongTimestamp, originalPingTimestamp] = event.data.split(':');
-      const currentTimestamp = new Date().getTime();
-      const ping = currentTimestamp - parseInt(originalPingTimestamp);
-      webSocketPing.value = ping;
-    }
-  };
-
-  const setupWebSocket = () => {
-    ws = new WebSocket('wss://brostonksnode-6745321f7dfb.herokuapp.com');
-
-    ws.onopen = () => {
-      console.log('WebSocket connection opened');
-      ws.send('startFetching');
+const startPingInterval = () => {
+  if (!pingInterval) {
+    pingInterval = setInterval(() => {
       pingWebSocket();
-      webSocketStatus.value = 'WebSocket connection opened';
-      startPingInterval();
-    };
+    }, 1000);
+  }
+};
 
-    ws.onclose = () => {
-      console.log('WebSocket connection closed');
-      stopPingInterval();
-    };
+const stopPingInterval = () => {
+  if (pingInterval) {
+    clearInterval(pingInterval);
+    pingInterval = null;
+  }
+};
 
-    ws.onmessage = handlePongMessage;
+const handlePongMessage = (event) => {
+  if (event.data.startsWith('pong:')) {
+    const [, pongTimestamp, originalPingTimestamp] = event.data.split(':');
+    const currentTimestamp = new Date().getTime();
+    const ping = currentTimestamp - parseInt(originalPingTimestamp);
+    webSocketPing.value = ping;
+  }
+};
+
+const setupWebSocket = () => {
+  ws = new WebSocket('wss://brostonksnode-6745321f7dfb.herokuapp.com');
+
+  ws.onopen = () => {
+    console.log('WebSocket connection opened');
+    ws.send('startFetching');
+    pingWebSocket();
+    webSocketStatus.value = 'WebSocket connection opened';
+    startPingInterval();
   };
 
-  onMounted(() => {
-    setupWebSocket();
-  });
+  ws.onclose = () => {
+    console.log('WebSocket connection closed');
+    stopPingInterval();
+  };
 
-  const { idle, reset } = useIdle(10000);
+  ws.onmessage = handlePongMessage;
+};
 
-  watch(idle, (newIdleValue) => {
-    console.log('Idle value changed:', newIdleValue);
+setupWebSocket(); // Call setupWebSocket immediately when the plugin is loaded
 
-    if (!newIdleValue) {
-      if (ws.readyState !== WebSocket.OPEN) {
-        console.log('Reconnecting WebSocket...');
-        setupWebSocket();
-        webSocketStatus.value = 'WebSocket connection reopened';
-      }
-    } else {
-      if (ws.readyState === WebSocket.OPEN) {
-        ws.close();
-      }
-      stopPingInterval();
-      console.log('WebSocket connection closed due to inactivity');
-      webSocketStatus.value = 'WebSocket connection closed due to inactivity';
+const { idle } = useIdle(10000);
+
+watch(idle, (newIdleValue) => {
+  console.log('Idle value changed:', newIdleValue);
+
+  if (!newIdleValue) {
+    if (ws.readyState !== WebSocket.OPEN) {
+      console.log('Reconnecting WebSocket...');
+      setupWebSocket();
+      webSocketStatus.value = 'WebSocket connection reopened';
     }
-  });
-
-  onBeforeUnmount(() => {
+  } else {
     if (ws.readyState === WebSocket.OPEN) {
       ws.close();
     }
     stopPingInterval();
-  });
-  
+    console.log('WebSocket connection closed due to inactivity');
+    webSocketStatus.value = 'WebSocket connection closed due to inactivity';
+  }
+});
+
+onBeforeUnmount(() => {
+  if (ws.readyState === WebSocket.OPEN) {
+    ws.close();
+  }
+  stopPingInterval();
+});
+
+export default ({ app }) => {
   app.setupWebSocket = setupWebSocket;
 };
