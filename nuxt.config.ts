287dee6baa13c441dcf3d785f46d13400948d02b
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-08-31 22:10:15 -0400

    2 files modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,6 +27,13 @@ export default defineNuxtConfig({
       bodyAttrs: {
         class: 'h-full bg-white dark:bg-gray-900 antialiased',
       },
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/@widgetbot/crate@3',
+          async: true,
+          defer: true,
+        },
+      ],
     },
     keepalive: true,
   },
--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,12 @@
 </template>
 
 <script setup lang="ts">
-
+onMounted(() => {
+  new Crate({
+    server: '1146980018174578688', // BroStonks
+    channel: '1146980018631737386', // #general
+  });
+});
 
 definePageMeta({
   colorMode: 'dark',
