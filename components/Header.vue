422d194b918bf788e71b90a39a7b7244e47b97ba
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-09-01 15:36:52 -0400

    1 file modified

--- components/Header.vue
+++ components/Header.vue
@@ -36,6 +36,7 @@
         <div class="flex items-center lg:order-2">
           <a
             href="#"
+            @click.prevent="handleLoginWithDiscord"
             class="
               text-gray-800
               dark:text-white
@@ -146,3 +147,34 @@
     </nav>
   </header>
 </template>
+
+<script setup>
+const supabase = useSupabaseClient();
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/';
+  // Make sure to include `https://` when not localhost.
+  url = url.includes('http') ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+  return url;
+};
+
+const handleLoginWithDiscord = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: 'discord',
+    options: {
+      redirectTo: getURL(),
+    },
+  });
+
+  if (error) {
+    // Handle the error, such as displaying an error message.
+    console.error('Error signing in with Discord:', error.message);
+  }
+};
+
+console.log(useSupabaseUser());
+</script>
