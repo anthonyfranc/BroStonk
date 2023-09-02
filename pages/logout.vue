b3211f68b569fa5a00101b33a5b3f1cb73b1c51b
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-09-02 16:26:16 -0400

    1 file modified

--- pages/logout.vue
+++ pages/logout.vue
@@ -1,13 +1,12 @@
 <script setup>
 const supabase = useSupabaseClient();
 
-try {
-  const { error } = await supabase.auth.signOut();
-  if (error) {
-    // Handle the error, such as displaying an error message.
-    console.error('Error signing out with Discord:', error.message);
-  }
-} finally {
+const { error } = await supabase.auth.signOut();
+if (error) {
+  // Handle the error, such as displaying an error message.
+  console.error('Error signing out with Supabase:', error.message);
+} else {
+  // Sign-out was successful, navigate to the root path.
   navigateTo({ path: '/' });
 }
 </script>
