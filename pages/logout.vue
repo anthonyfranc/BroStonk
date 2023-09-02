cc49232935edb63c6df8fac9e69f39b78193687c
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-09-02 16:15:52 -0400

    1 file added

---
+++ pages/logout.vue
@@ -0,0 +1,11 @@
+<script setup>
+try {
+  const { error } = await supabase.auth.signOut();
+  if (error) {
+    // Handle the error, such as displaying an error message.
+    console.error('Error signing out with Discord:', error.message);
+  }
+} finally {
+  navigateTo({ path: '/' });
+}
+</script>
