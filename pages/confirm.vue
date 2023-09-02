bf6f99776ad237c2a7935ae6d90cddb2aa672f9b
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-09-02 19:40:35 -0400

    1 file modified

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -3,10 +3,12 @@
 </template>
 
 <script setup>
+import { initFlowbite } from 'flowbite';
 const supabase = useSupabaseClient();
 
 supabase.auth.onAuthStateChange((event, session) => {
   if (event == 'SIGNED_IN') {
+    initFlowbite();
     navigateTo({ path: '/' });
   }
 });
