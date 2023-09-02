1c849827fdbe59ecb8e5f533d59afa7e6e17fcd7
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-09-02 14:30:29 -0400

    1 file modified

--- app.vue
+++ app.vue
@@ -10,22 +10,38 @@
 <script setup>
 import { useWebSocket } from '~/composables/useWebSocket.js';
 
+import {
+  initAccordions,
+  initCarousels,
+  initCollapses,
+  initDials,
+  initDismisses,
+  initDrawers,
+  initDropdowns,
+  initModals,
+  initPopovers,
+  initTabs,
+  initTooltips,
+} from 'flowbite';
+
 const { webSocketStatus, webSocketPing } = useWebSocket();
 const show = ref(false);
 
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initAccordions();
+  initCarousels();
+  initCollapses();
+  initDials();
+  initDismisses();
+  initDrawers();
+  initDropdowns();
+  initModals();
+  initPopovers();
+  initTabs();
+  initTooltips();
+});
+
 provide('webSocketStatus', webSocketStatus);
 provide('webSocketPing', webSocketPing);
-</script>
-
-<script>
-export default {
-  // This option will be called every time the route changes
-  beforeEnter: async (to, from, next) => {
-    // Call initFlowbite
-    await initFlowbite();
-
-    // Continue with the route change
-    next();
-  },
-};
 </script>
