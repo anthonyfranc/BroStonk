e55f9b96e7832453aba82ecc917d63a0a4f4e9f7
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-09-02 14:12:35 -0400

    1 file modified

--- middleware/route.global.js
+++ middleware/route.global.js
@@ -1,8 +1,9 @@
 // middleware/flowbite.js
 
 import { initFlowbite } from 'flowbite';
-import { useRoute } from 'vue-router';
 
 export default function () {
-  initFlowbite();
+  onUpdated(() => {
+    initFlowbite();
+  });
 }
