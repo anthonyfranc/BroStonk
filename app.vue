8ec5ce85ae2f65429610b99ffc15f0f0d9c3e16b
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-09-02 14:38:21 -0400

    1 file modified

--- app.vue
+++ app.vue
@@ -16,7 +16,7 @@ const { webSocketStatus, webSocketPing } = useWebSocket();
 const show = ref(false);
 const nuxtApp = useNuxtApp();
 
-nuxtApp.hook('page:start', () => {
+nuxtApp.hook('render:html', () => {
   initFlowbite();
 });
 
