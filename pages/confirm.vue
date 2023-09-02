c6a269d73a7f07dbd777a2c600aa2424139af149
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-09-02 19:49:00 -0400

    1 file modified

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -1,15 +1,3 @@
 <template>
   <div></div>
 </template>
-
-<script setup>
-import { initFlowbite } from 'flowbite';
-const supabase = useSupabaseClient();
-
-supabase.auth.onAuthStateChange((event, session) => {
-  if (event == 'SIGNED_IN') {
-    initFlowbite();
-    navigateTo({ path: '/confirm' });
-  }
-});
-</script>
