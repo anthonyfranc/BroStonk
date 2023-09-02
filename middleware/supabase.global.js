157432ec5c50180aee6cb5098450e18244a87409
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-09-02 19:54:55 -0400

    1 file modified

--- middleware/supabase.global.js
+++ middleware/supabase.global.js
@@ -1,6 +1,5 @@
-const supabase = useSupabaseClient();
-
 export default defineNuxtRouteMiddleware(() => {
+  const supabase = useSupabaseClient();
   supabase.auth.onAuthStateChange((event, session) => {
     if (event == 'SIGNED_IN') {
       initFlowbite();
