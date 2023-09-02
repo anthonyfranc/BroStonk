41b9a4a61720325e60ca0d01b3086bb2c6fd36d6
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-09-01 22:10:19 -0400

    1 file modified

--- components/Header.vue
+++ components/Header.vue
@@ -79,7 +79,6 @@
                   absolute
                   w-3.5
                   h-3.5
-                  bg-green-400
                   border-2 border-white
                   dark:border-gray-800
                   rounded-full
