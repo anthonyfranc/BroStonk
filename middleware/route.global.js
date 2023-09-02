b84583e80e99f0bb5576de2c1853ea62617a8ce8
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-09-02 14:07:43 -0400

    1 file modified

--- middleware/route.global.js
+++ middleware/route.global.js
@@ -1,5 +1,16 @@
-export default defineNuxtPlugin(() => {
-  onNuxtReady(async () => {
+// middleware/flowbite.js
+
+import { initFlowbite } from 'flowbite';
+import { useRoute } from 'vue-router';
+
+export default function () {
+  const route = useRoute();
+
+  // Check if the redirect from Supabase occurred (modify this condition as needed)
+  const redirectFromSupabase = route.fullPath.includes('brostonks.com?code=');
+
+  // Initialize Flowbite if it hasn't been initialized and the redirect occurred
+  if (redirectFromSupabase) {
     initFlowbite();
-  });
-});
+  }
+}
