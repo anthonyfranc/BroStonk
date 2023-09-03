c788ed1868b45ea8c07af9d36be9bad7d4730497
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-09-02 20:22:24 -0400

    1 file modified

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -1,3 +1,15 @@
 <template>
-  <div></div>
+  <statusBar />
+  <div class="mx-auto max-w-screen-xl">
+    <Header />
+    <Stats />
+    <StatsTable />
+  </div>
+  <Footer />
 </template>
+
+<script setup>
+definePageMeta({
+  colorMode: 'dark',
+});
+</script>
