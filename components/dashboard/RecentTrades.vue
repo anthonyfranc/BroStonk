5d8d657b7c180586f52892448a3566d82eebf3bf
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-09-13 21:49:37 -0400

    1 file modified

--- components/dashboard/RecentTrades.vue
+++ components/dashboard/RecentTrades.vue
@@ -24,22 +24,22 @@
         lg:px-8
       "
     >
-      <div class="flex items-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="#ff6a00"
-          class="w-4 h-4 mr-1.5"
-        >
-          <path
-            d="M11.983 1.907a.75.75 0 00-1.292-.657l-8.5 9.5A.75.75 0 002.75 12h6.572l-1.305 6.093a.75.75 0 001.292.657l8.5-9.5A.75.75 0 0017.25 8h-6.572l1.305-6.093z"
-          />
-        </svg>
+<div class="flex items-center relative">
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="#ff6a00"
+    class="w-4 h-4 mr-1.5"
+  >
+    <path
+      d="M11.983 1.907a.75.75 0 00-1.292-.657l-8.5 9.5A.75.75 0 002.75 12h6.572l-1.305 6.093a.75.75 0 001.292.657l8.5-9.5A.75.75 0 0017.25 8h-6.572l1.305-6.093z"
+    />
+  </svg>
 
-        <h2 class="text-base font-semibold leading-7 text-white">
-          Recent Trades
-        </h2>
-      </div>
+  <h2 class="text-base font-semibold leading-7 text-white">
+    Recent Trades
+  </h2>
+</div>
     </header>
     <ul role="list">
       <TransitionRoot :show="true" class="divide-y divide-white/5">
@@ -54,7 +54,7 @@
             <li class="px-4 py-4 sm:px-6 lg:px-8">
               <div class="flex items-center gap-x-3">
                 <img
-                  src=""
+                  :src="item.cryptoImage"
                   alt=""
                   class="h-6 w-6 flex-none rounded-full bg-gray-800"
                 />
@@ -91,32 +91,11 @@
                   </div>
                 </h3>
                 <time class="flex-none text-xs text-gray-600"
-                  >10 seconds ago</time
+                  >{{formatRelativeTime(item.date)}}</time
                 >
               </div>
               <p class="mt-3 truncate text-sm text-gray-500 flex space-x-2.5">
-<div class="text-center">
-  <div
-    class="
-      relative
-      rounded-md
-      bg-gray-700/40
-      px-2
-      py-1.5
-      my-1
-      text-xs
-      font-medium
-      text-gray-400
-      ring-1 ring-inset ring-white/10
-    "
-  >
-    <div class="absolute -top-2 left-2 text-xs font-medium text-gray-300">
-      Hash
-    </div>
-    {{item.hash}}
-  </div>
-</div>
-<div class="text-center">
+<div class="text-left">
   <div
     class="
       relative
@@ -132,12 +111,12 @@
     "
   >
     <div class="relative text-left text-xs font-medium text-gray-300">
-      Blockchain
+      Value
     </div>
-    {{item.blockchain}}
+    {{formatToUSD(item.value_usd)}}
   </div>
 </div>
-<div class="text-center">
+<div class="text-left">
   <div
     class="
       relative
@@ -153,9 +132,30 @@
     "
   >
     <div class="relative text-left text-xs font-medium text-gray-300">
-      Blockchain
+      Amount
     </div>
-    {{item.blockchain}}
+    {{item.token_amount}}
+  </div>
+</div>
+<div class="text-left">
+  <div
+    class="
+      relative
+      rounded-md
+      bg-gray-700/40
+      px-2
+      py-1.5
+      my-1
+      text-xs
+      font-medium
+      text-gray-400
+      ring-1 ring-inset ring-white/10
+    "
+  >
+    <div class="relative text-left text-xs font-medium text-gray-300">
+      Price
+    </div>
+    {{formatToUSD(item.token_price)}}
   </div>
 </div>
 
@@ -214,6 +214,34 @@ const addItemToPayloadArray = (newValue) => {
   }
 };
 
+function formatRelativeTime(timestamp) {
+  const now = new Date().getTime();
+  const secondsAgo = Math.floor((now - timestamp) / 1000);
+
+  if (secondsAgo < 60) {
+    return `${secondsAgo} second${secondsAgo !== 1 ? 's' : ''} ago`;
+  } else if (secondsAgo < 3600) {
+    const minutesAgo = Math.floor(secondsAgo / 60);
+    return `${minutesAgo} minute${minutesAgo !== 1 ? 's' : ''} ago`;
+  } else if (secondsAgo < 86400) {
+    const hoursAgo = Math.floor(secondsAgo / 3600);
+    return `${hoursAgo} hour${hoursAgo !== 1 ? 's' : ''} ago`;
+  } else if (secondsAgo < 604800) {
+    const daysAgo = Math.floor(secondsAgo / 86400);
+    return `${daysAgo} day${daysAgo !== 1 ? 's' : ''} ago`;
+  } else {
+    const date = new Date(timestamp);
+    return date.toLocaleDateString('en-US');
+  }
+}
+
+function formatToUSD(number) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  }).format(number);
+}
+
 // Function to uppercase item.asset based on its length
 const uppercaseAsset = (asset) => {
   if (asset.length < 4) {
@@ -232,17 +260,44 @@ const uppercaseType = (type) => {
 // Use debounce to control the rate at which items are added
 const debouncedAddItem = debounce(addItemToPayloadArray, 1000); // Adjust the debounce delay as needed
 
+let cryptoData = ref({}); // Initialize cryptoData as a ref object with crypto names as keys
+
+(async () => {
+  const { data: loadedCryptoData, error: cryptoError } = await supabase
+    .from('crypto')
+    .select('name, logo');
+
+  if (!cryptoError && loadedCryptoData) {
+    loadedCryptoData.forEach((crypto) => {
+      cryptoData.value[crypto.name] = crypto;
+    });
+  }
+})();
+
 const trades = supabase
   .channel('custom-insert-channel')
-  .on(
-    'postgres_changes',
-    { event: 'INSERT', schema: 'public', table: 'trades' },
-    (payload) => {
-      const newValue = payload.new;
+  .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'trades' }, (payload) => {
+    const newValue = payload.new;
+    // Check if cryptoData is defined
+    if (cryptoData) {
+      //console.log(cryptoData._value[newValue.asset])
+      // Get the associated crypto data based on 'crypto.name' (or 'trades.asset')
+      const cryptoItem = cryptoData.value[newValue.asset];
 
-      // Call the debounced function to add the item
-      debouncedAddItem(newValue);
+      if (cryptoItem) {
+        // Add the crypto image to the trade data
+        newValue.cryptoImage = cryptoItem.logo;
+
+        // Call the debounced function to add the item with the crypto image
+        debouncedAddItem(newValue);
+      } else {
+        console.warn('Crypto item not found for trade:', newValue['asset']);
+      }
+    } else {
+      //console.warn('Crypto data is not available.');
     }
-  )
+  })
   .subscribe();
 </script>
+
+
